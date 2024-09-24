a4e105381c93dd8e85ec22cf540bd5260bf2dab4
Felipe Ranzoni Borges <felipe.ranzoni@hotmail.com>
2024-09-24 08:56:31 -0300

    Rota para enviar e-mail de alteração e rota para alterar de fato o e-mail do usuário

--- AutenticacaoDoisFatores/Controllers/UsuarioController.cs
+++ AutenticacaoDoisFatores/Controllers/UsuarioController.cs
@@ -64,6 +64,43 @@ namespace AutenticacaoDoisFatores.Controllers
             }
         }
 
+        [HttpPut("AlterarEmail/{id}")]
+        public async Task<ActionResult> AlterarEmailAsync(int id, UsuarioAlterarEmail usuarioAlterar)
+        {
+            try
+            {
+                var urlBase = RetornarUrlFormatada("Usuario/ConfirmarAlteracaoEmail/");
+                var retorno = await _servico.EnviarEmailAlteracaoEmailAsync(id, usuarioAlterar, urlBase);
+
+                return Sucesso("Um e-mail de confirmação foi enviado");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("ConfirmarAlteracaoEmail/{token}")]
+        public async Task<ActionResult> ConfirmarAlteracaoEmailAsync(string token)
+        {
+            try
+            {
+                var retorno = await _servico.AlterarEmailAsync(token);
+                if (retorno is null)
+                    return MensagemHtml("Falha", "Não encontrado", "Não foi encontrado um usuário com estes dados.");
+
+                return MensagemHtml("Confirmação", "Confirmação de alteração", "A alteração foi realizada com sucesso!");
+            }
+            catch (SecurityTokenExpiredException)
+            {
+                return MensagemHtml("Falha", "Link expirado", "Será necessário solicitar um novo link");
+            }
+            catch (Exception e)
+            {
+                return MensagemHtml("Falha", "Falha ao completar a solicitação", $"Por favor, entre em contato com o responsável pelo sistema. Erro: {e.Message}");
+            }
+        }
+
         [HttpPost("Autenticar")]
         public async Task<ActionResult<UsuarioAutenticado?>> AutenticarAsync(UsuarioAutenticar usuarioAutenticar)
         {
